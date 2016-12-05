8215377397fae9b0a45acfc9511773de90f7d59a
Tareq Anwar <trqnwr@gmail.com>
2016-12-05 16:36:50 -0400

    Added projects section and fixed bugs

--- scripts/app.js
+++ scripts/app.js
@@ -3,9 +3,13 @@ $(document).ready(function() {
     var scrollEnd = 0;
     var hOffset = $('#start-navbar-dark').offset();
     var aOffset = $('#start-about-animation').offset();
+    var tOffset = $('#start-technologies-animation').offset();
+    var pOffset = $('#start-projects').offset();
     // Optimization flags so that it doesnt run every scroll
-    var aboutAnimate = true;
     var headerAnimate = true;
+    var aboutAnimate = true;
+    var technologiesAnimate = true;
+    var projectsAnimate = true;
 
     $(document).scroll(function() {
         scrollStart = $(this).scrollTop();
@@ -16,18 +20,31 @@ $(document).ready(function() {
         } else if (scrollStart === hOffset.top) {
             $('.navbar').css('background', 'none');
             headerAnimate = true;
-        }
-        if (scrollEnd > aOffset.top && aboutAnimate) {
+        } else if (scrollEnd > aOffset.top && aboutAnimate) {
+            console.log(aOffset);
             $('.about p, .about h4').addClass('animated fadeInUp');
             $('.about h3').addClass('animated fadeInDown');
+            $('.profilepic').addClass('animated fadeInUp');
             aboutAnimate = false;
+        } else if (scrollEnd > tOffset.top && technologiesAnimate) {
+            $('.techsquares img').addClass('animated fadeInUp');
+            projectsAnimate = false;
+        } else if (scrollEnd > pOffset.top && projectsAnimate) {
+            $('.f3').addClass('animated fadeInUp');
+            $('.s3').addClass('animated fadeInUp');
+            projectsAnimate = false;
         }
     });
 
     $('.about-nav').click(function() {
-        console.log("Clicked!");
         $('html, body').animate({
             scrollTop: $('.about-section').offset().top
+        }, 1300);
+    });
+
+    $('.projects-nav').click(function() {
+        $('html, body').animate({
+            scrollTop: $('.projects').offset().top
         }, 1300);
     });
 });
