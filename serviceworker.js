d2e876b5de69019861aba220d70ec8e51b64c193
Tareq Anwar <trqnwr@gmail.com>
2018-10-23 23:36:21 -0500

    updated static assets

--- serviceworker.js
+++ serviceworker.js
@@ -2,10 +2,9 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox
 
 const staticAssets = [
     './',
-    './css',
-    './images',
-    './scripts',
-    './images/dog_fetching_ball.jpg'
+    './css/styles.css',
+    './images/tareq-anwar.jpg',
+    './images/pexels-photo.jpg'
 ];
 
 // Shows logs, warnings and errors.
@@ -19,6 +18,21 @@ workbox.routing.registerRoute(
     new RegExp(/.*\.(png|jpg|jpeg|gif)/),
     workbox.strategies.cacheFirst({
         cacheName: 'image-cache',
+        plugins: [
+            new workbox.expiration.Plugin({
+                // Only cache requests for 24 hours
+                maxAgeSeconds: 24 * 60 * 60,
+                // Only cache 20 requests.
+                maxEntries: 20,
+            }),
+        ]
+    })
+);
+
+workbox.routing.registerRoute(
+    new RegExp(/.*\.(js)/),
+    workbox.strategies.cacheFirst({
+        cacheName: 'js-cache',
         plugins: [
             new workbox.expiration.Plugin({
                 // Only cache requests for 24 hours
